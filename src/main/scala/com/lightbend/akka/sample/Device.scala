7253448d85fd7acd773bba662921429f4dc9dcf3
sbogdanchuk <napali777@gmail.com>
2020-01-19 02:38:17 +0300

    feat: Add device actor with tests

---
+++ src/main/scala/com/lightbend/akka/sample/Device.scala
@@ -0,0 +1,42 @@
+package com.lightbend.akka.sample
+
+import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
+import akka.actor.typed.{ActorRef, Behavior, PostStop, Signal}
+
+object Device {
+  def apply(groupId: String, deviceId: String): Behavior[Command] =
+    Behaviors.setup(context => new Device(context, groupId, deviceId))
+
+  sealed trait Command
+  final case class ReadTemperature(requestId: Long, replyTo: ActorRef[RespondTemperature]) extends Command
+  final case class RespondTemperature(requestId: Long, value: Option[Double])
+
+  final case class RecordTemperature(requestId: Long, value: Double, replyTo: ActorRef[TemperatureRecorded]) extends Command
+  final case class TemperatureRecorded(requestId: Long)
+}
+
+class Device(context: ActorContext[Device.Command], groupId: String, deviceId: String)
+  extends AbstractBehavior[Device.Command](context){
+  import Device._
+
+  var lastTemperatureReading: Option[Double] = None
+
+  context.log.info("Device actor {}-{} started", groupId, deviceId)
+
+  override def onMessage(msg: Command): Behavior[Command] = msg match {
+    case RecordTemperature(id, value, replyTo) =>
+      context.log.info("Recorded temperature reading {} with {}", value, id)
+      lastTemperatureReading = Some(value)
+      replyTo ! TemperatureRecorded(id)
+      this
+
+    case ReadTemperature(id, replyTo) =>
+      replyTo ! RespondTemperature(id, lastTemperatureReading)
+      this
+  }
+
+  override def onSignal: PartialFunction[Signal, Behavior[Command]] = {
+    case PostStop => context.log.info("Device actor {}-{} stopped", groupId, deviceId)
+      this
+  }
+}
