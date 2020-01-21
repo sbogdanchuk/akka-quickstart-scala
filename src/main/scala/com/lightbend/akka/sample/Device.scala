5f279cd2d04b0e03048bec295a3406a15e8ceacf
sbogdanchuk <napali777@gmail.com>
2020-01-21 15:18:02 +0300

    feat: Add DeviceManager and finish actors hierarchy

--- src/main/scala/com/lightbend/akka/sample/Device.scala
+++ src/main/scala/com/lightbend/akka/sample/Device.scala
@@ -8,11 +8,14 @@ object Device {
     Behaviors.setup(context => new Device(context, groupId, deviceId))
 
   sealed trait Command
+
   final case class ReadTemperature(requestId: Long, replyTo: ActorRef[RespondTemperature]) extends Command
   final case class RespondTemperature(requestId: Long, value: Option[Double])
 
   final case class RecordTemperature(requestId: Long, value: Double, replyTo: ActorRef[TemperatureRecorded]) extends Command
   final case class TemperatureRecorded(requestId: Long)
+
+  case object Passivate extends Command
 }
 
 class Device(context: ActorContext[Device.Command], groupId: String, deviceId: String)
@@ -33,6 +36,8 @@ class Device(context: ActorContext[Device.Command], groupId: String, deviceId: S
     case ReadTemperature(id, replyTo) =>
       replyTo ! RespondTemperature(id, lastTemperatureReading)
       this
+
+    case Passivate => Behaviors.stopped
   }
 
   override def onSignal: PartialFunction[Signal, Behavior[Command]] = {
